5179a7b10c06e51dd1312fe6b1bd826ff7bb6885
Muhammed anzarshah <anzarsha3240@gmail.com>
2025-11-18 16:01:10 +0530

    fix:typescript error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,12 @@ import History from "./pages/History";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<SignUp />} />
-         <Route path="/user"  element={<User />} />
-         <Route path="/history" element={<History />} />
-         
-
-
-      </Routes>
+    <Routes>
+      <Route index element={<User />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
   );
 }
 
